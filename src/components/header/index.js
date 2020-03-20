f9ee9ed8e800f4c6af30ea095cc09eef748655a9
Lars den Bakker <larsdenbakker@gmail.com>
2020-03-20 20:32:23 +0100

    Add totals card

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,72 +6,12 @@ import twitterIcon from '../../assets/icons/twitter.svg';
 import downloadIcon from '../../assets/icons/download.svg';
 
 const Header = () => (
-  <header class={'navbar is-transparent is-fixed-top' + style.header}>
-    <div class="navbar-brand">
-      <a class="navbar-item" href="/">
-        <img src={logo} alt="Lockdown, information about current lockdown state of different countries" width="112" height="28" />
-      </a>
-      <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
+  <header class={style.header}>
+    <a href="">
+      <img src={logo} alt="Lockdown, information about current lockdown state of different countries" width="112" height="28" />
+    </a>
 
-    <div id="navbarExampleTransparentExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item" href="/">
-          Home
-        </a>
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link" href="./docs.html">
-            Docs
-          </a>
-          <div class="navbar-dropdown is-boxed">
-            <a class="navbar-item" href="./overview.html">
-              Overview
-            </a>
-            <a class="navbar-item" href="./sources.html">
-              Sources
-            </a>
-            <hr class="navbar-divider" />
-            <a class="navbar-item" href="./about.html">
-              About
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="field is-grouped">
-            <p class="control">
-              <a
-                class="bd-tw-button button"
-                data-social-network="Twitter"
-                data-social-action="tweet"
-                data-social-target="https://code-for-all.github.io/lockdown"
-                target="_blank"
-                href="https://twitter.com/intent/tweet?text=Look at the lockdown state of countries across the world&amp;url=https://code-for-all.github.io/lockdown&amp;via=codeforall"
-              >
-                <span class="icon">
-                  <img class={style.icon} src={twitterIcon} />
-                </span>
-                <span>Tweet</span>
-              </a>
-            </p>
-            <p class="control">
-              <a class="button is-primary" href="https://github.com/Code-for-All/lockdown/raw/master/data/datafile.json">
-                <span class="icon">
-                  <img class={style.icon} src={downloadIcon} />
-                </span>
-                <span>Download</span>
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <h1 class={style.heading}>COVID-19 WORLD LOCKDOWNS</h1>
   </header>
 );
 
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,10 +1,12 @@
 import { h } from 'preact';
 import WorldMap from '../../components/world-map';
+import Totals from '../../components/totals';
 import style from './style';
 
 const Home = () => (
   <div class={style.home}>
     <WorldMap />
+    <Totals />
   </div>
 );
 
